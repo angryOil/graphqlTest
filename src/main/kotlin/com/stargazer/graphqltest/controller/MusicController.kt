e0bbc59c405768bd1705ea94ffc1f6163298121a
jeong-on-yu <jipmj13@gmail.com>
2024-05-13 17:50:26 +0900

    graphql 관련 추가

---
+++ src/main/kotlin/com/stargazer/graphqltest/controller/MusicController.kt
@@ -0,0 +1,24 @@
+package com.stargazer.graphqltest.controller
+
+import com.stargazer.graphqltest.entity.Music
+import com.stargazer.graphqltest.service.MusicService
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("/music")
+class MusicController(
+    private val musicService: MusicService,
+) {
+
+    @QueryMapping
+    fun music(@Argument title: String): List<Music> {
+        return musicService.findMusic(title)
+    }
+
+    @MutationMapping
+    fun createMusic(@Argument title: String, @Argument artist: String): Long {
+        return musicService.registerMusic(title, artist)
+    }
+}
